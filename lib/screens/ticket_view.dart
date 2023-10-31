6b478bb72ada454a10e8186954a5e18a50dc5641
Eusebio Joveth <yurijoveth@gmail.com>
2023-10-31 18:33:42 +0100

    implementando widget ticket_view

--- lib/screens/ticket_view.dart
+++ lib/screens/ticket_view.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/rendering.dart';
 import 'package:jt_reserva/utils/app_layout.dart';
 import 'package:jt_reserva/utils/app_styles.dart';
 import 'package:jt_reserva/widgets/thick_container.dart';
